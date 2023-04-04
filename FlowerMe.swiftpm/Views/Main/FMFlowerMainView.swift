3ac1af0393c2472ed9cc5e2a62b4442cf680fa88
valselee <sollleky72@gmail.com>
2023-04-04 17:20:03 +0900

    [Feat] 기본 카드 뷰에 flower 전달 후 Section Header 까지 구현, CardView 디자인시스템 간단하게 정리

--- FlowerMe.swiftpm/Views/Main/FMFlowerMainView.swift
+++ FlowerMe.swiftpm/Views/Main/FMFlowerMainView.swift
@@ -8,7 +8,44 @@
 import SwiftUI
 
 struct FMFlowerMainView: View {
+    @ObservedObject var flowerVM: FlowerVM
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            ForEach(flowerVM.flowerList, id: \.id) { flower in
+                LazyVStack(
+                    alignment: .leading,
+                    pinnedViews: .sectionHeaders
+                ) {
+                    Section {
+                        ForEach(flower.bloomingSeason, id: \.self) { season in
+                            Text(season)
+                        }
+                    } header: {
+                        HStack {
+                            Text(flower.name)
+                                .font(.largeTitle)
+                            Spacer()
+                            // 이 꽃으로 보냈던 편지의 갯수를 보여주면 어때
+                            Text("posts: \(0)")
+                        }
+                        .padding(.vertical, 24)
+                        .frame(alignment: .leading)
+                        .background {
+                            LinearGradient(
+                                colors: [
+                                    Color(.systemBackground),
+                                    Color(.systemBackground).opacity(0.4),
+                                    Color(.systemBackground).opacity(0.1),
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 32)
     }
 }
--- FlowerMe.swiftpm/Views/Main/FMMainView.swift
+++ FlowerMe.swiftpm/Views/Main/FMMainView.swift
@@ -8,15 +8,41 @@
 import SwiftUI
 
 struct FMMainView: View {
+    @ObservedObject var flowerVM: FlowerVM = FlowerVM()
+    @State private var isNavigated: Bool = false
+    
     var body: some View {
         TabView {
             ForEach(Constants.INTRO_ONBOARDING, id: \.self) { text in
-                FMCustomCardView(style: .large()) {
-                    Text(text)
-                        .font(.title2)
+                if text != "" {
+                    FMCustomCardView(style: .large()) {
+                        Text(text)
+                            .font(.title2)
+                    }
+                } else {
+                    NavigationLink {
+                        TabView {
+                            FMFlowerMainView(flowerVM: flowerVM)
+                                .navigationBarBackButtonHidden()
+                                .tabItem {
+                                    Label("home", systemImage: "house")
+                                }
+                            
+                            Text("편지통")
+                                .tabItem {
+                                    Label("post", systemImage: "signpost.right")
+                                }
+                        }
+                        
+                    } label: {
+                        FMCustomCardView(style: .mini()) {
+                            Text("Pick a Flower for Yourself")
+                        }
+                    }
                 }
             }
         }
         .tabViewStyle(.page)
     }
 }
+
