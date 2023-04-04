f013f2eef59fdd48374e2443f4693509e4e553de
valselee <sollleky72@gmail.com>
2023-04-04 17:19:23 +0900

    [Feat] 모델 선언, 뷰 모델에 더미 데이터 3개 생성

--- FlowerMe.swiftpm/Models/Flower.swift
+++ FlowerMe.swiftpm/Models/Flower.swift
@@ -5,4 +5,44 @@
 //  Created by Celan on 2023/04/04.
 //
 
-import Foundation
+import SwiftUI
+
+struct Flower: Hashable {
+    var id: String = UUID().uuidString
+    var name: String
+    var bloomingSeason: [String]
+    var languageOfFlowers: String
+    var footer: String
+    var image: String
+    
+    var tintColor: Color {
+        // TODO: 색 구체화하기
+        if bloomingSeason.contains(Constants.FlowerSeasons.SPRING) {
+            return .yellow
+        } else if bloomingSeason.contains(Constants.FlowerSeasons.SUMMER) {
+            return .orange
+        } else if bloomingSeason.contains(Constants.FlowerSeasons.FALL) {
+            return .brown
+        } else if bloomingSeason.contains(Constants.FlowerSeasons.WINTER) {
+            return .white
+        }
+        
+        return .black
+    }
+    
+    init(
+        name: String,
+        languageOfFlowers: String,
+        footer: String,
+        image: String,
+        bloomingSeason: String...
+    ) {
+        self.name = name
+        self.bloomingSeason = bloomingSeason
+        self.languageOfFlowers = languageOfFlowers
+        self.footer = footer
+        self.image = image
+    }
+}
+
+
--- FlowerMe.swiftpm/ViewModels/FlowerVM.swift
+++ FlowerMe.swiftpm/ViewModels/FlowerVM.swift
@@ -5,4 +5,30 @@
 //  Created by Celan on 2023/04/04.
 //
 
-import Foundation
+import SwiftUI
+
+final class FlowerVM: ObservableObject {
+    @Published var flowerList: [Flower] = [
+        .init(
+            name: "Red Rose",
+            languageOfFlowers: "I Love You",
+            footer: "The madness of love the greatest of heaven’s blessings.",
+            image: "rose",
+            bloomingSeason: Constants.FlowerSeasons.SPRING, Constants.FlowerSeasons.SUMMER, Constants.FlowerSeasons.FALL
+        ),
+        .init(
+            name: "Basil",
+            languageOfFlowers: "Wishes Good Luck",
+            footer: "Chance favors the prepared mind.",
+            image: "basil",
+            bloomingSeason: Constants.FlowerSeasons.SUMMER
+        ),
+        .init(
+            name: "Dianthus",
+            languageOfFlowers: "Be Gallant",
+            footer: "He who is brave is free.",
+            image: "dianthus",
+            bloomingSeason: Constants.FlowerSeasons.SPRING, Constants.FlowerSeasons.SUMMER
+        ),
+    ]
+}
