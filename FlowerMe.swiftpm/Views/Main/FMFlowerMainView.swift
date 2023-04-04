c53684a394ad14536626529158cfdd54399a64b9
valselee <sollleky72@gmail.com>
2023-04-04 17:18:11 +0900

    [Add] Flower, FlowerVM, MainView 파일 생성

---
+++ FlowerMe.swiftpm/Views/Main/FMFlowerMainView.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Celan on 2023/04/04.
+//
+
+import SwiftUI
+
+struct FMFlowerMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
