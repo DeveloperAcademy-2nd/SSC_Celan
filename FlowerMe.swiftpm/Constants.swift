974ae271303808d416289b8f1ebb7dfa98d3810f
valselee <sollleky72@gmail.com>
2023-04-04 19:06:41 +0900

    [Chore] 자잘한 텍스트 변경

--- FlowerMe.swiftpm/Constants.swift
+++ FlowerMe.swiftpm/Constants.swift
@@ -11,8 +11,7 @@ enum Constants {
     static let INTRO_ONBOARDING: [String] = [
         "Welcome to FlowerMe, \ncheer yourself from today to tomorrow.",
         "Have you ever heard about the sustainable growth? \nJust like you, Our planet earth has sustainable Flowers.",
-        "If you send Flower Letters to yourself, \nYou can find out language of flowers and the sustainabiilty.",
-        ""
+        "If you send Flower Letters to yourself, \nYou can find out language of flowers and the sustainabiilty."
     ]
     
     enum FlowerSeasons {
