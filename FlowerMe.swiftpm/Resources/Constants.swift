fa1fc243e24c8183d7ee838428d8d410087b437d
valselee <sollleky72@gmail.com>
2023-04-05 11:05:06 +0900

    [Update] 파일 경로 수정 및 이름 변경

--- FlowerMe.swiftpm/Resources/Constants.swift
+++ FlowerMe.swiftpm/Resources/Constants.swift
@@ -11,12 +11,13 @@ enum Constants {
     static let INTRO_ONBOARDING: [String] = [
         "Welcome to FlowerMe, \ncheer yourself from today to tomorrow.",
         "Have you ever heard about the sustainable growth? \nJust like you, Our planet earth has sustainable Flowers.",
-        "If you send Flower Letters to yourself, \nYou can find out language of flowers and the sustainabiilty."
+        "If you send Flower Letters to yourself, \nYou can find out language of flowers and the sustainabiilty.",
+        ""
     ]
     
     enum FlowerSeasons {
         static let SPRING: String = "Spring"
-        static let SUMMER: String = "Sumemer"
+        static let SUMMER: String = "Summer"
         static let FALL: String = "Fall"
         static let WINTER: String = "Winter"
     }
