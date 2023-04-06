3a6b0754dd717320298bfe3962bf94d63a39764f
valselee <sollleky72@gmail.com>
2023-04-06 11:00:50 +0900

    [Feat] .show 모디파이어에서 사용할 뷰모델의 Publihsed 속성 구현

--- FlowerMe.swiftpm/ViewModels/FlowerVM.swift
+++ FlowerMe.swiftpm/ViewModels/FlowerVM.swift
@@ -30,5 +30,29 @@ final class FlowerVM: ObservableObject {
             image: "dianthus",
             bloomingSeason: Constants.FlowerSeasons.SPRING, Constants.FlowerSeasons.SUMMER
         ),
+        .init(
+            name: "Red Rose",
+            languageOfFlowers: "I Love You",
+            footer: "The madness of love the greatest of heaven’s blessings.",
+            image: "rose",
+            bloomingSeason: Constants.FlowerSeasons.SPRING, Constants.FlowerSeasons.SUMMER, Constants.FlowerSeasons.FALL
+        ),
+        .init(
+            name: "Basil",
+            languageOfFlowers: "Wishes Good Luck",
+            footer: "Chance favors the prepared mind.",
+            image: "basil",
+            bloomingSeason: Constants.FlowerSeasons.SUMMER
+        ),
+        .init(
+            name: "Dianthus",
+            languageOfFlowers: "Be Gallant",
+            footer: "He who is brave is free.",
+            image: "dianthus",
+            bloomingSeason: Constants.FlowerSeasons.SPRING, Constants.FlowerSeasons.SUMMER
+        ),
     ]
+    
+    @Published var currentFlower: Flower?
+    @Published var isDetailCardViewDisplayed: Bool = false
 }
