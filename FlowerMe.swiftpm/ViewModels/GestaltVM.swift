baed021fa9fd25d031baaabff4b110d7630293a3
valselee <sollleky72@gmail.com>
2023-04-07 17:06:21 +0900

    [Feat] 클리어 관련 내용을 보관하는 배열 구현, Closure 퍼즐의 .sink 구현

--- FlowerMe.swiftpm/ViewModels/GestaltVM.swift
+++ FlowerMe.swiftpm/ViewModels/GestaltVM.swift
@@ -6,8 +6,49 @@
 //
 
 import SwiftUI
+import Combine
 
 final class GestaltVM: ObservableObject {
     @Published var clearedPrinciples = [String]()
-    @Published var isFirstDisplayedDict: [String: Bool] = [:]
+    
+    @Published var isFirstDisplayedClosure: Bool = false
+    @Published var isFirstDisplayedContinuation: Bool = false
+    @Published var isFirstDisplayedFigrueGround: Bool = false
+    @Published var isFirstDisplayedProximity: Bool = false
+    @Published var isFirstDisplayedSimilarity: Bool = false
+    @Published var isFirstDisplayedPragnanz: Bool = false
+    
+    // 4 True == Clear
+    @Published var closurePuzzle: [Bool] = []
+    @Published var closurePuzzleCleared: Bool = false
+    
+    // Tap to Clear
+    @Published var proximityPuzzleCleared: Bool = false
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addClosurePuzzleSubscriber()
+    }
+    
+    /**
+     내부의 Published 결과 리스트가 변화할 때마다 receiveValue를 확인하고 조건에 따라 특정 속성을 재할당합니다.
+     */
+    private func addClosurePuzzleSubscriber() {
+        self.$closurePuzzle
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(_):
+                    break
+                }
+            } receiveValue: { [weak self] results in
+                print(results)
+                if results.count == 4 {
+                    self?.closurePuzzleCleared = true
+                }
+            }
+            .store(in: &cancellables)
+    }
 }
