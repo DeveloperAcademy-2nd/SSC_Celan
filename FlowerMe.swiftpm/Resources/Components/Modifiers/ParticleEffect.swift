dd5b10ee7e7227806b7b44598da28c8a053addbf
valselee <sollleky72@gmail.com>
2023-04-08 23:33:48 +0900

    [Feat] 꽃이 배경에서 펑 터지는 효과 추가

--- FlowerMe.swiftpm/Resources/Components/Modifiers/ParticleEffect.swift
+++ FlowerMe.swiftpm/Resources/Components/Modifiers/ParticleEffect.swift
@@ -1,20 +1,48 @@
 //
-//  SwiftUIView.swift
-//  
+//  ParticleEffect.swift
+//  partTest
 //
-//  Created by Celan on 2023/04/07.
+//  Created by Celan on 2023/03/29.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ParticlesModifier: ViewModifier {
+    @State var time = 0.0
+    @State var scale = 0.1
+    let duration = 5.0
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            ForEach(0..<80, id: \.self) { index in
+                content
+                    .modifier(FireworkParticlesGeometryEffect(time: time))
+                    .opacity(((duration-time) / duration))
+            }
+        }
+        .onAppear {
+            withAnimation (.easeOut(duration: duration)) {
+                self.time = duration
+                self.scale = 1.0
+            }
+        }
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
+struct FireworkParticlesGeometryEffect: GeometryEffect {
+    var time: Double
+    var speed = Double.random(in: 20 ... 200)
+    var direction = Double.random(in: -Double.pi ...  Double.pi)
+    
+    var animatableData: Double {
+        get { time }
+        set { time = newValue }
+    }
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let xTranslation = speed * cos(direction) * time
+        let yTranslation = speed * sin(direction) * time
+        let affineTranslation = CGAffineTransform(translationX: xTranslation, y: yTranslation)
+        return ProjectionTransform(affineTranslation)
     }
 }
