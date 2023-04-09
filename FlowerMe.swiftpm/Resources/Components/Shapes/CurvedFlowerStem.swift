c9e374a08686fb408de10a089f684a5b8beeb2f7
valselee <sollleky72@gmail.com>
2023-04-09 18:34:41 +0900

    [Feat] #5 특정 Path를 따라서 드래그할 수 있도록 Shape, Moving 구현

--- FlowerMe.swiftpm/Resources/Components/Shapes/CurvedFlowerStem.swift
+++ FlowerMe.swiftpm/Resources/Components/Shapes/CurvedFlowerStem.swift
@@ -7,14 +7,49 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CurvedFlowerStem: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            
+            path.addCurve(
+                to: .init(x: rect.width, y: rect.height),
+                control1: .init(x: 0, y: rect.height * 0.25),
+                control2: .init(x: rect.width * 1.5, y: rect.height * 0.75)
+            )
+        }
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
+struct CurvedFlowerStemReversed: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: -(rect.width), y: rect.height))
+            
+            path.addCurve(
+                to: .zero,
+                control1: .init(x: -(rect.width * 1.5), y: rect.height * 0.75),
+                control2: .init(x: 0, y: rect.height * 0.25)
+            )
+        }
+    }
+}
+
+struct Moving: AnimatableModifier {
+    var time: CGFloat
+    let path: Path
+    var start: CGPoint = .init(x: 0, y: 0)
+    var isReversed: Bool = false
+    
+    var animatableData: CGFloat {
+        get { time }
+        set { time = newValue }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .position(
+                path.trimmedPath(from: 0, to: time).currentPoint ?? start
+            )
     }
 }
