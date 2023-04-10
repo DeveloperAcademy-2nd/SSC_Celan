b485c53851f0d979e84ab20349a8d0cabdfc873c
valselee <sollleky72@gmail.com>
2023-04-10 23:08:32 +0900

    [Feat] #5 작은 꽃들을 그릴 수 있도록 radian 과 함께 구현

--- FlowerMe.swiftpm/Resources/Components/FMMiniFlower.swift
+++ FlowerMe.swiftpm/Resources/Components/FMMiniFlower.swift
@@ -7,14 +7,53 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct MiniFlower: View {
+    let tintColor: Color
+    let budColor: Color
+    let radian: CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            MiniPetal()
+                .rotationEffect(
+                    .radians(radian * 5)
+                )
+            
+            MiniPetal()
+                .rotationEffect(
+                    .radians(radian * 2)
+                )
+            
+            MiniPetal()
+                .rotationEffect(
+                    .radians(radian * 3)
+                )
+            
+            MiniPetal()
+                .rotationEffect(
+                    .radians(radian * 4)
+                )
+            
+            MiniPetal()
+                .rotationEffect(
+                    .radians(radian)
+                )
+            
+            Circle()
+                .fill(budColor)
+                .frame(width: 20, height: 20)
+                .shadow(radius: 15)
+        }
+        
+        
     }
-}
-
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
+    
+    private func MiniPetal() -> some View {
+        Ellipse()
+            .fill(tintColor)
+            .frame(
+                width: 25,
+                height: 60
+            )
     }
 }
