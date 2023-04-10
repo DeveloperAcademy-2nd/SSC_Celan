ec26d90dbea2368d46d57922d4d784ddd73477ef
valselee <sollleky72@gmail.com>
2023-04-10 23:33:52 +0900

    [Feat] #5 서로 다른 꽃잎 반복해서 유사성 원리 설명 중

--- FlowerMe.swiftpm/Views/Gestalt/SimilarityView.swift
+++ FlowerMe.swiftpm/Views/Gestalt/SimilarityView.swift
@@ -8,7 +8,29 @@
 import SwiftUI
 
 struct SimilarityView: View {
+    let gridItem: [GridItem] = Array(
+        repeating: .init(
+            .flexible(),
+            spacing: 10
+        ),
+        count: 8
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: gridItem) {
+            ForEach(0..<88) { index in
+                MiniFlower(
+                    tintColor: index >= 48 ? Color("SpringTint") : Color("SummerTint"),
+                    budColor: index >= 48 ? Color.white : Color.yellow,
+                    radian: index >= 48 ? 2 : 15
+                )
+            }
+        }
+    }
+}
+
+struct SimilaryityPreview: PreviewProvider {
+    static var previews: some View {
+        SimilarityView()
     }
 }
