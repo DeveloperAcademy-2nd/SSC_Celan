d162bbc3c3d9b766d1ce9a93b4430df8735324b4
valselee <sollleky72@gmail.com>
2023-04-12 01:14:47 +0900

    [Feat] #5 Ellipse Shape을 trim 하여 다른 사이즈의 꽃 제작 완료

--- FlowerMe.swiftpm/Resources/Components/FMFlower.swift
+++ FlowerMe.swiftpm/Resources/Components/FMFlower.swift
@@ -7,14 +7,71 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct FMFlower: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Group {
+                FlowerPetal()
+                    .rotationEffect(.degrees(150))
+                FlowerPetal()
+                    .rotationEffect(.degrees(170))
+                FlowerPetal()
+                    .rotationEffect(.degrees(190))
+                FlowerPetal()
+                    .rotationEffect(.degrees(130))
+                FlowerPetal()
+                    .rotationEffect(.degrees(110))
+                FlowerPetal()
+                    .rotationEffect(.degrees(100))
+                FlowerPetal()
+                    .rotationEffect(.degrees(70))
+                FlowerPetal()
+                    .rotationEffect(.degrees(50))
+                FlowerPetal()
+                    .rotationEffect(.degrees(30))
+            }
+           
+           
+            FlowerPetal()
+                .rotationEffect(.degrees(210))
+            FlowerPetal()
+                .rotationEffect(.degrees(240))
+            FlowerPetal()
+                .rotationEffect(.degrees(250))
+            FlowerPetal()
+                .rotationEffect(.degrees(280))
+            FlowerPetal()
+                .rotationEffect(.degrees(290))
+            FlowerPetal()
+                .rotationEffect(.degrees(310))
+            FlowerPetal()
+                .rotationEffect(.degrees(330))
+            FlowerPetal()
+                .rotationEffect(.degrees(360))
+            
+            Circle()
+                .fill(Color.yellow)
+                .frame(width: 90)
+                .shadow(radius: 10)
+        }
+    }
+}
+
+struct FlowerPetal: View {
+    var body: some View {
+        Ellipse()
+            .trim(from: 0.25, to: 0.75)
+            .fill(.red)
+            .frame(
+                width: 200,
+                height: 60
+            )
+           
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        FMFlower()
     }
 }
