f8f6be7f6b0f01031d212e2c8a4359f1763d8dde
valselee <sollleky72@gmail.com>
2023-04-15 00:32:53 +0900

    [Feat] #5 꽃잎 수를 init 호출하면서 지정하고, Particle 효과를 부여할 꽃들을 구분

--- FlowerMe.swiftpm/Resources/Components/Modifiers/ParticleEffect.swift
+++ FlowerMe.swiftpm/Resources/Components/Modifiers/ParticleEffect.swift
@@ -11,10 +11,21 @@ struct ParticlesModifier: ViewModifier {
     @State var time = 0.0
     @State var scale = 0.1
     let duration = 5.0
+    let numberOfParticles: Int?
+    
+    init(
+        time: Double = 0.0,
+        scale: Double = 0.1,
+        numberOfParticles: Int? = 80
+    ) {
+        self.time = time
+        self.scale = scale
+        self.numberOfParticles = numberOfParticles
+    }
     
     func body(content: Content) -> some View {
         ZStack {
-            ForEach(0..<80, id: \.self) { index in
+            ForEach(0..<numberOfParticles!, id: \.self) { index in
                 content
                     .modifier(FireworkParticlesGeometryEffect(time: time))
                     .opacity(((duration-time) / duration))
