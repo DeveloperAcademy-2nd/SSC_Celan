6815bc4a05269dd5b7dce1d4f483966a56b97384
valselee <sollleky72@gmail.com>
2023-04-14 23:19:57 +0900

    [Feat] #5 다섯 꽃잎 Shape 완성, scale로 크기 조절

--- FlowerMe.swiftpm/Resources/Components/Shapes/FivePetalsFlower.swift
+++ FlowerMe.swiftpm/Resources/Components/Shapes/FivePetalsFlower.swift
@@ -7,14 +7,50 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
+struct FivePetalsFlower: Shape {
+    var petalOffset: CGFloat = -60
+    var petalWidth: CGFloat = 150
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // 5 petals
+        for number in stride(
+            from: 0,
+            to: Double.pi,
+            by: Double.pi / 5
+        ) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(
+                rotationAngle: number * 2
+            )
+            
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(
+                CGAffineTransform(
+                    translationX: rect.width / 2,
+                    y: rect.height / 2
+                )
+            )
+            
+            // create a path for petal using our properties plus a fixed Y and height
+            let originalPetal = Path(
+                ellipseIn: CGRect(
+                    x: petalOffset,
+                    y: 0,
+                    width: petalWidth,
+                    height: rect.width / 4
+                )
+            )
+            
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+        
+        // now send the main path back
+        return path
     }
 }
