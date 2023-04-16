e5978997218df070df3d513b07d445b5b6f9a4e6
valselee <sollleky72@gmail.com>
2023-04-16 20:36:38 +0900

    [Feat] #5 HalfFlower가 뷰모델의 데이터에 접근하고 소통할 수 있도록 수정, 그라디언트 배경색 구현

--- FlowerMe.swiftpm/Resources/Components/Shapes/HalfFlowerView.swift
+++ FlowerMe.swiftpm/Resources/Components/Shapes/HalfFlowerView.swift
@@ -8,27 +8,84 @@
 import SwiftUI
 
 struct HalfFlowerView: View {
+    @ObservedObject var gestaltVM: GestaltVM
     @Binding var topLeadingPetal: Double
     @Binding var bottomLeadingLeaf: Double
     
     var body: some View {
         VStack(spacing: 100) {
             Petal()
+                .fill(
+                    gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE) || gestaltVM.isClosurePuzzleDone
+                    ? LinearGradient(
+                        gradient:
+                            Gradient(
+                                colors: [
+                                    Color(red: 245/255, green: 35/255, blue: 47/255),
+                                    Color(red: 255/255, green: 214/255, blue: 158/255)
+                                ]
+                            ),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                    : LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color.black
+                        ]),
+                        startPoint: .top,
+                        endPoint: .top
+                    )
+                )
                 .frame(width: 400, height: 400)
             // 270
-                .rotationEffect(.degrees(topLeadingPetal))
+                .rotationEffect(
+                    .degrees(
+                        gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE)
+                        ? 270
+                        : topLeadingPetal
+                    )
+                )
                 .onTapGesture {
                     if Int(topLeadingPetal) % 270 != 0 {
                         withAnimation {
                             topLeadingPetal += 45.0
+                            if Int(topLeadingPetal) % 270 == 0 {
+                                gestaltVM.closurePuzzle.append(true)
+                            }
                         }
                     }
                 }
             
             Leaf()
+                .fill(
+                    gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE) || gestaltVM.isClosurePuzzleDone
+                    ? LinearGradient(
+                        gradient: Gradient(
+                            colors: [
+                                Color(red: 117/255, green: 211/255, blue: 76/255),
+                                Color(red: 38/255, green: 164/255, blue: 27/255)
+                            ]
+                        ),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                    : LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color.black
+                        ]),
+                        startPoint: .top,
+                        endPoint: .top
+                    )
+                )
                 .frame(width: 200, height: 200)
             // 180
-                .rotationEffect(.degrees(bottomLeadingLeaf))
+                .rotationEffect(
+                    .degrees(
+                        gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE)
+                        ? 180
+                        : bottomLeadingLeaf
+                    )
+                )
                 .rotation3DEffect(
                     .degrees(180),
                     axis: (x: -1, y: 1, z: 0)
@@ -37,6 +94,9 @@ struct HalfFlowerView: View {
                     if Int(bottomLeadingLeaf) % 180 != 0 {
                         withAnimation {
                             bottomLeadingLeaf += 36.0
+                            if Int(bottomLeadingLeaf) % 180 == 0 {
+                                gestaltVM.closurePuzzle.append(true)
+                            }
                         }
                     }
                 }
@@ -45,27 +105,84 @@ struct HalfFlowerView: View {
 }
 
 struct HalfFlowerReversedView: View {
+    @ObservedObject var gestaltVM: GestaltVM
     @Binding var topTrailingPetal: Double
     @Binding var bottomTrailingLeaf: Double
     
     var body: some View {
         VStack(spacing: 100) {
             Petal()
+                .fill(
+                    gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE) || gestaltVM.isClosurePuzzleDone
+                    ? LinearGradient(
+                        gradient:
+                            Gradient(
+                                colors: [
+                                    Color(red: 245/255, green: 35/255, blue: 47/255),
+                                    Color(red: 255/255, green: 214/255, blue: 158/255)
+                                ]
+                            ),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                    : LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color.black
+                        ]),
+                        startPoint: .top,
+                        endPoint: .top
+                    )
+                )
                 .frame(width: 400, height: 400)
             // 270
-                .rotationEffect(.degrees(topTrailingPetal))
+                .rotationEffect(
+                    .degrees(
+                        gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE)
+                        ? 270
+                        : topTrailingPetal
+                    )
+                )
                 .onTapGesture {
                     if Int(topTrailingPetal) % 270 != 0 || topTrailingPetal == 0 {
                         withAnimation {
                             topTrailingPetal += 135.0
+                            if Int(topTrailingPetal) % 270 == 0 {
+                                gestaltVM.closurePuzzle.append(true)
+                            }
                         }
                     }
                 }
             
             Leaf()
+                .fill(
+                    gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE) || gestaltVM.isClosurePuzzleDone
+                    ? LinearGradient(
+                        gradient: Gradient(
+                            colors: [
+                                Color(red: 117/255, green: 211/255, blue: 76/255),
+                                Color(red: 38/255, green: 164/255, blue: 27/255)
+                            ]
+                        ),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                    : LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color.black
+                        ]),
+                        startPoint: .top,
+                        endPoint: .top
+                    )
+                )
                 .frame(width: 200, height: 200)
             // 180
-                .rotationEffect(.degrees(bottomTrailingLeaf))
+                .rotationEffect(
+                    .degrees(
+                        gestaltVM.clearedPrinciples.contains(Constants.Gestalt.CLOSURE)
+                        ? 180
+                        : bottomTrailingLeaf
+                    )
+                )
                 .rotation3DEffect(
                     .degrees(180),
                     axis: (x: -1, y: 1, z: 0)
@@ -74,6 +191,9 @@ struct HalfFlowerReversedView: View {
                     if Int(bottomTrailingLeaf) % 180 != 0 || bottomTrailingLeaf == 0 {
                         withAnimation {
                             bottomTrailingLeaf += 60.0
+                            if Int(bottomTrailingLeaf) % 180 == 0, bottomTrailingLeaf != 0 {
+                                gestaltVM.closurePuzzle.append(true)
+                            }
                         }
                     }
                 }
